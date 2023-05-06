2d67ad585df9a5b6147fb412ff48a698ee445e21
ngoclbph26472 <ngoclbph26472@fpt.edu.vn>
2023-05-06 22:44:11 +0700

    First Commit

---
+++ DA1/src/main/java/com/poly/it17323/group6/service/ipml/KhuyenMaiService.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poly.it17323.group6.service.ipml;
+
+import com.poly.it17323.group6.domainmodel.KhuyenMai;
+import com.poly.it17323.group6.repository.KhuyenMaiRepository;
+import com.poly.it17323.group6.response.KhuyenMaiReponse;
+import com.poly.it17323.group6.service.IKhuyenMaiService;
+import java.sql.Date;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ *
+ * @author Admin
+ */
+public class KhuyenMaiService {
+
+//    private KhuyenMaiRepository kmrepo = new KhuyenMaiRepository();
+//
+//    @Override
+//    public List<KhuyenMai> getAll() {
+//        return kmrepo.getAll();
+//    }
+//
+//    @Override
+//    public boolean add(KhuyenMaiReponse km) {
+//        Date ngayBatDau = Date.valueOf(km.getNgayBD());
+//        Date ngayKetThuc = Date.valueOf(km.getNgayKT());
+//        Date ngayTao = Date.valueOf(km.getNgayTao());
+//        Date ngaySua = Date.valueOf(km.getNgaySua());
+//        kmrepo.add(new KhuyenMai(null, km.getMaKM(), km.getTenKM(), ngayBatDau, ngayKetThuc, km.getMoTa(), km.getGiamGia(), km.getTinhTrang(), ngayTao, ngaySua));
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean update(KhuyenMaiReponse km) {
+//        Date ngayBatDau = Date.valueOf(km.getNgayBD());
+//        Date ngayKetThuc = Date.valueOf(km.getNgayKT());
+//        Date ngayTao = Date.valueOf(km.getNgayTao());
+//        Date ngaySua = Date.valueOf(km.getNgaySua());
+//        kmrepo.update(new KhuyenMai(km.getIdKM(), km.getMaKM(), km.getTenKM(), ngayBatDau, ngayKetThuc, km.getMoTa(), km.getGiamGia(), km.getTinhTrang(), ngayTao, ngaySua));
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean delete(KhuyenMaiReponse km) {
+//        Date ngayBatDau = Date.valueOf(km.getNgayBD());
+//        Date ngayKetThuc = Date.valueOf(km.getNgayKT());
+//        Date ngayTao = Date.valueOf(km.getNgayTao());
+//        Date ngaySua = Date.valueOf(km.getNgaySua());
+//        kmrepo.delete(new KhuyenMai(km.getIdKM(), km.getMaKM(), km.getTenKM(), ngayBatDau, ngayKetThuc, km.getMoTa(), km.getGiamGia(), km.getTinhTrang(), ngayTao, ngaySua));
+//        return true;
+//    }
+//
+//    @Override
+//    public KhuyenMaiReponse getOne(UUID id) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//    }
+//
+//    @Override
+//    public String MaKm() {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//    }
+//
+//    @Override
+//    public List<KhuyenMai> get_By_Name(String name) {
+//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//    }
+
+}
